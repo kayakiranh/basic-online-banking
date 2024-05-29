1a49d8e16aec6ee47adb32f2e5ba4d40d3ae4adb
Hüseyin <huseyin.kayakiran@gmail.com>
2024-05-29 12:07:02 +0300

    Development : Ocelot ApiGateway

--- OnlineBankingApp/ApiGateways/Ocelot.ApiGateway/Program.cs
+++ OnlineBankingApp/ApiGateways/Ocelot.ApiGateway/Program.cs
@@ -1,15 +1,15 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Configuration.AddJsonFile($"ocelot.json", false, true);
+builder.Services.AddOcelot(builder.Configuration);
 
 WebApplication app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseOcelot().Wait();
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
-app.Run();
+app.UseRouting();
